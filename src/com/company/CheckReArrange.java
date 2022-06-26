69e8f374eb81577ccafea8760fbddcc04a8665d7
Yashpal Bansal <yashb234@gmail.com>
2022-06-26 23:56:22 +0530

    CheckReArrange

--- src/com/company/CheckReArrange.java
+++ src/com/company/CheckReArrange.java
@@ -8,16 +8,28 @@ public class CheckReArrange {
         int s_index = 0;
         int l_index = arr.length - 1;
 
-        for(int i = 0; i< arr.length; i++){
-            if(arr[i]<0){
-                arr2[s_index++] = arr[i];
-            }else{
-                arr2[l_index--] = arr[i];
-            }
-        }
+//        for(int i = 0; i< arr.length; i++){
+//            if(arr[i]<0){
+//                arr2[s_index++] = arr[i];
+//            }else{
+//                arr2[l_index--] = arr[i];
+//            }
+//        }
+//
+//        for(int i = 0; i < arr.length; i ++){
+//            arr[i] = arr2[i];
+//        }
 
-        for(int i = 0; i < arr.length; i ++){
-            arr[i] = arr2[i];
+        while(s_index<l_index){
+            if(arr[s_index]>0){
+                while(arr[l_index] > 0 && s_index<l_index){
+                    l_index--;
+                }
+                int temp = arr[l_index];
+                arr[l_index] = arr[s_index];
+                arr[s_index] = temp;
+            }
+            s_index++;
         }
 
     }
