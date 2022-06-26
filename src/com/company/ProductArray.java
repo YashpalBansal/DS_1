61068af9fe6f05f511e35c2fa9408e472b1ed7c2
Yashpal Bansal <yashb234@gmail.com>
2022-06-26 21:59:34 +0530

    ProductArray

--- src/com/company/ProductArray.java
+++ src/com/company/ProductArray.java
@@ -2,7 +2,19 @@ package com.company;
 
 public class ProductArray {
     public static int[] findProduct(int arr[]){
-        return arr;
+
+        int[] productArray = new int[arr.length];
+
+        for(int i=0; i<arr.length; i++){
+            int product = 1;
+            for(int j=0; j<arr.length;j++){
+                if(j!=i) {
+                    product=product*arr[j];
+                }
+            }
+            productArray[i] = product;
+        }
+        return productArray;
     }
 
     public static String arrayToString(int arr[]){
